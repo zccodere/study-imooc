ef2f8631c607e23a9c6517e10da6c6f960abc24d
zccodere <zccoder@aliyun.com>
2017-06-16 00:50:37 +0800

    新增-慕课网《SpringBoot进阶之Web进阶》学习源码

---
+++ 07-my-myspringbootweb/src/main/java/com/myimooc/springbootweb/MyspringbootApplication.java
@@ -0,0 +1,12 @@
+package com.myimooc.springbootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringbootApplication.class, args);
+	}
+}
