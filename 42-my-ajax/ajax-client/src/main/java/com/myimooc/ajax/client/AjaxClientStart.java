e6b8d64a8a1c7f2e350e21100a3ebd32514736c5
zccodere <zccoder@aliyun.com>
2018-04-19 21:22:04 +0800

    新增-慕课网《Ajax跨域完全讲解》学习源码

---
+++ 42-my-ajax/ajax-client/src/main/java/com/myimooc/ajax/client/AjaxClientStart.java
@@ -0,0 +1,12 @@
+package com.myimooc.ajax.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaxClientStart {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjaxClientStart.class, args);
+	}
+}
