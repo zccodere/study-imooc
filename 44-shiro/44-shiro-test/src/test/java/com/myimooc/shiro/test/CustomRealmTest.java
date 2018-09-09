848c6a551ad613a0448eaff3a485981d7a2965b1
zccodere <zccoder@aliyun.com>
2018-09-09 17:04:33 +0800

    新增-慕课网《Shiro安全框架入门》学习源码

---
+++ 44-shiro/44-shiro-test/src/test/java/com/myimooc/shiro/test/CustomRealmTest.java
@@ -0,0 +1,49 @@
+package com.myimooc.shiro.test;
+
+import com.myimooc.shiro.test.realm.CustomRealm;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.junit.Test;
+
+/**
+ * <br>
+ * 标题: 自定义Realm测试<br>
+ * 描述: 测试自定义Realm<br>
+ *
+ * @author zc
+ * @date 2018/05/02
+ */
+public class CustomRealmTest {
+
+    @Test
+    public void  testAuthentication(){
+        CustomRealm customRealm = new CustomRealm();
+
+        // 构建 SecurityManager
+        DefaultSecurityManager defaultSecurityManager = new DefaultSecurityManager();
+        defaultSecurityManager.setRealm(customRealm);
+
+        HashedCredentialsMatcher matcher = new HashedCredentialsMatcher();
+        matcher.setHashAlgorithmName("md5");
+        matcher.setHashIterations(1);
+
+        customRealm.setCredentialsMatcher(matcher);
+
+        // 主体提交认证请求
+        SecurityUtils.setSecurityManager(defaultSecurityManager);
+        Subject subject = SecurityUtils.getSubject();
+
+        UsernamePasswordToken token = new UsernamePasswordToken("Mark","123456");
+        subject.login(token);
+
+        // 是否认证
+        System.out.println("isAuthenticated:" + subject.isAuthenticated());
+
+        subject.checkRole("admin");
+        subject.checkPermission("user:delete");
+        subject.checkPermission("user:add");
+    }
+}
---
+++ 44-shiro/44-shiro-web/src/main/java/com/myimooc/shiro/web/cache/RedisCache.java
@@ -0,0 +1,82 @@
+package com.myimooc.shiro.web.cache;
+
+import org.apache.shiro.cache.Cache;
+import org.apache.shiro.cache.CacheException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.util.SerializationUtils;
+
+import java.util.Collection;
+import java.util.Set;
+
+/**
+ * <br>
+ * 标题: 自定义redis缓存实体类<br>
+ * 描述: 实现Cache接口<br>
+ *
+ * @author zc
+ * @date 2018/05/03
+ */
+@Component
+public class RedisCache<K, V> implements Cache<K, V> {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    private final String CACHE_PREFIX = "shiro-cache:";
+
+    private byte[] getKey(K k) {
+        if (k instanceof String) {
+            return (CACHE_PREFIX + k).getBytes();
+        }
+        return SerializationUtils.serialize(k);
+    }
+
+    @Override
+    public V get(K k) throws CacheException {
+        System.out.println("从redis中获取权限数据");
+        Object value = redisTemplate.opsForValue().get(getKey(k));
+        if (value != null) {
+            return (V) value;
+        }
+        return null;
+    }
+
+    @Override
+    public V put(K k, V v) throws CacheException {
+        redisTemplate.opsForValue().set(k, v, 600);
+        return v;
+    }
+
+    @Override
+    public V remove(K k) throws CacheException {
+        byte[] key = getKey(k);
+        byte[] value = (byte[]) redisTemplate.opsForValue().get(key);
+        redisTemplate.delete(key);
+        if (value != null) {
+            return (V) SerializationUtils.deserialize(value);
+        }
+        return null;
+    }
+
+    @Override
+    public void clear() throws CacheException {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Set<K> keys() {
+        return null;
+    }
+
+    @Override
+    public Collection<V> values() {
+        return null;
+    }
+}
