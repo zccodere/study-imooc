ad74b284451dd796d0e5192350da5a8856323bc9
zccodere <zccoder@aliyun.com>
2018-09-11 22:17:25 +0800

    新增：慕课网《Java消息中间件》学习源码

---
+++ 27-activemq/27-activemq-boot/src/main/java/com/myimooc/activemq/boot/producer/ProducerService.java
@@ -0,0 +1,23 @@
+package com.myimooc.activemq.boot.producer;
+
+/**
+ * 消息发送服务
+ *
+ * @author ZhangCheng on 2017-07-22
+ */
+public interface ProducerService {
+
+    /**
+     * 功能：发送字符串到消息服务-队列模式
+     *
+     * @param message 消息
+     */
+    void sendMessageQueue(String message);
+
+    /**
+     * 功能：发送字符串到消息服务-主题模式
+     *
+     * @param message 消息
+     */
+    void sendMessageTopic(String message);
+}
