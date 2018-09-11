ff3a55c0a5f41cb1bba3f6754db433f7ad56e252
zccodere <zccoder@aliyun.com>
2018-09-11 20:58:44 +0800

    新增：慕课网《基于SSH实现员工管理系统》相关课程学习源码

---
+++ 30-ssh/30-ssh-employee/src/main/java/com/myimooc/ssh/employee/dao/DepartmentDao.java
@@ -0,0 +1,66 @@
+package com.myimooc.ssh.employee.dao;
+
+import java.util.List;
+
+import com.myimooc.ssh.employee.domain.Department;
+
+/**
+ * 部门管理的DAO的接口
+ *
+ * @author ZhangCheng on 2017-08-18
+ */
+public interface DepartmentDao {
+
+    /**
+     * 查询总数
+     *
+     * @return 总数
+     */
+    int findCount();
+
+    /**
+     * 分页查询
+     *
+     * @param begin    开始
+     * @param pageSize 每页大小
+     * @return 部门集合
+     */
+    List<Department> findByPage(int begin, int pageSize);
+
+    /**
+     * 保存
+     *
+     * @param department 部门
+     */
+    void save(Department department);
+
+    /**
+     * 查询
+     *
+     * @param did 部门ID
+     * @return 部门
+     */
+    Department findById(Integer did);
+
+    /**
+     * 更新
+     *
+     * @param department 部门
+     */
+    void update(Department department);
+
+    /**
+     * 删除
+     *
+     * @param department 部门
+     */
+    void delete(Department department);
+
+    /**
+     * 查询全部
+     *
+     * @return 部门集合
+     */
+    List<Department> findAll();
+
+}
---
+++ 30-ssh/30-ssh-simple/src/main/java/com/myimooc/ssh/simple/domain/Product.java
@@ -0,0 +1,49 @@
+package com.myimooc.ssh.simple.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * 商品类
+ * @author ZhangCheng on 2017-08-16
+ */
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue
+	private Integer pid;
+	
+	private String pname;
+	
+	private Double price;
+	
+	@Override
+	public String toString() {
+		return "Product [pid=" + pid + ", pname=" + pname + ", price=" + price + "]";
+	}
+
+	public Integer getPid() {
+		return pid;
+	}
+
+	public void setPid(Integer pid) {
+		this.pid = pid;
+	}
+
+	public String getPname() {
+		return pname;
+	}
+
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+}
