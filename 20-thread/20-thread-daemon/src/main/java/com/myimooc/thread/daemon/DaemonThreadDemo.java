31553106f2ba66b0efc7561e75a4e79ff04fb0f3
zccodere <zccoder@aliyun.com>
2018-09-13 22:06:14 +0800

    新增：慕课网《细说多线程之Thread VS Runnable》学习源码

---
+++ 20-thread/20-thread-daemon/src/main/java/com/myimooc/thread/daemon/DaemonThreadDemo.java
@@ -0,0 +1,32 @@
+package com.myimooc.thread.daemon;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import java.util.Scanner;
+import java.util.concurrent.*;
+
+/**
+ * 主线程
+ *
+ * @author ZhangCheng on 2017-07-08
+ */
+public class DaemonThreadDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println("进入主线程" + Thread.currentThread().getName());
+
+        ThreadFactory threadFactory1 = new ThreadFactoryBuilder().setNameFormat("daemon-pool-%d").build();
+        ExecutorService executor = new ScheduledThreadPoolExecutor(1, threadFactory1);
+
+        executor.execute(new DaemonThread());
+
+        Scanner sc = new Scanner(System.in);
+        sc.next();
+        sc.close();
+
+        executor.shutdownNow();
+        System.out.println("退出主线程" + Thread.currentThread().getName());
+    }
+
+}
---
+++ 20-thread/20-thread-ticket/src/main/java/com/myimooc/thread/ticket/thread/TicketsThread.java
@@ -0,0 +1,40 @@
+package com.myimooc.thread.ticket.thread;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadFactory;
+
+/**
+ * 主类-启动线程类
+ *
+ * @author ZhangCheng on 2017-07-08
+ */
+public class TicketsThread {
+
+    public static void main(String[] args) {
+
+        // 使用线程池
+        ThreadFactory threadFactory1 = new ThreadFactoryBuilder().setNameFormat("window-pool-%d").build();
+        ExecutorService executor = new ScheduledThreadPoolExecutor(3, threadFactory1);
+
+        // 启用三个窗口卖票
+        int count = 3;
+        for (int i = 0; i < count; i++) {
+            executor.execute(new MyThread("窗口" + i));
+        }
+        executor.shutdown();
+
+//		创建三个线程，模拟三个窗口卖票
+//		MyThread mt1 = new MyThread("窗口1");
+//		MyThread mt2 = new MyThread("窗口2");
+//		MyThread mt3 = new MyThread("窗口3");
+//
+//		启动这三个线程，即窗口开始卖票
+//		mt1.start();
+//		mt2.start();
+//		mt3.start();
+    }
+
+}
