e7349bb7c177b994e7e7f4db44c2034e4d8b99c0
zccodere <zccoder@aliyun.com>
2020-03-15 21:26:53 +0800

    Update spring boot to 2.1.13.RELEASE

--- 08-freemarker/08-freemarker-simple/src/main/java/com/myimooc/freemarker/simple/controller/IndexController.java
+++ 08-freemarker/08-freemarker-simple/src/main/java/com/myimooc/freemarker/simple/controller/IndexController.java
@@ -1,10 +1,7 @@
-package com.myimooc.freemarker.simple.web.controller;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+package com.myimooc.freemarker.simple.controller;
 
 import com.myimooc.freemarker.simple.entity.User;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -12,46 +9,49 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 /**
- * 首页控制器
- * @author ZhangCheng
- * @date 2017-03-19
- * @version V1.0
+ * 首页控制层
+ *
+ * @author zc 2017-03-19
  */
 @Controller
 @RequestMapping("/")
 public class IndexController {
-    
-    private final static Logger LOGGER = LoggerFactory.getLogger(IndexController.class);
-    
+
+    private final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
     /**
      * 跳转到首页
-     * @return
      */
-    @GetMapping(value={"","/index"})
-    public ModelAndView doIndex(){
-        LOGGER.info("doIndex methed is run!");
-        
+    @GetMapping(value = {"", "/index"})
+    public ModelAndView doIndex() {
+        logger.info("doIndex method is run!");
+
         ModelAndView mv = new ModelAndView("/index");
-        mv.addObject("username","你好，Freemarker！");
-        mv.addObject("userList",this.prepareUserList());
+        mv.addObject("username", "你好，Freemarker！");
+        mv.addObject("userList", this.prepareUserList());
         return mv;
     }
-    
+
     /**
      * 准备用户数据
+     *
      * @return 用户数据
      */
-    private List<User> prepareUserList(){
-        List<User> list = new ArrayList<User>();
+    private List<User> prepareUserList() {
+        List<User> list = new ArrayList<>();
         int count = 5;
-        for(int i=0;i<count;i++){
+        for (int i = 0; i < count; i++) {
             User item = new User();
-            item.setName("用户"+i);
+            item.setName("用户" + i);
             item.setBirthday(new Date());
             list.add(item);
         }
         return list;
     }
-    
+
 }
