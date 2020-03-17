d049dd3092b706c2cd73e3a33ba7f2b3b40e3c87
zccodere <zccoder@aliyun.com>
2020-03-17 23:13:09 +0800

    Update hibernate to 5.3.15.Final

--- 23-hibernate/23-hibernate-mtm/src/main/java/com/myimooc/hibernate/mtm/util/HibernateUtil.java
+++ 23-hibernate/23-hibernate-mtm/src/main/java/com/myimooc/hibernate/mtm/util/HibernateUtil.java
@@ -1,55 +1,47 @@
 package com.myimooc.hibernate.mtm.util;
 
-/**
- * session会话工具类
- * @author ZhangCheng on 2017-07-11
- *
- */
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
+/**
+ * session会话工具类
+ *
+ * @author zc 2017-07-11
+ */
 public class HibernateUtil {
-	
-	private static SessionFactory sessionFactory;
-	
-	private static Session session;
 
-	static {
-		// 创建Configuration对象，读取hibernate.cfg.xml文件，完成初始化
-		Configuration config = new Configuration().configure();
-		StandardServiceRegistryBuilder ssrb = new StandardServiceRegistryBuilder()
-				.applySettings(config.getProperties());
-		StandardServiceRegistry ssr=ssrb.build();
-		sessionFactory=config.buildSessionFactory(ssr);
-	}
-	
-	/**
-	 * 获取SessionFactory
-	 * @return
-	 */
-	public static SessionFactory getSessionFactory(){
-		return sessionFactory;
-	}
-	
-	/**
-	 * 获取Session
-	 * @return
-	 */
-	public static Session getSession(){
-		session=sessionFactory.openSession();
-		return session;
-	}
-	
-	/**
-	 * 关闭Session
-	 * @param session
-	 */
-	public static void closeSession(Session session){
-		if(session!=null){
-			session.close();
-		}
-	}
+    private static SessionFactory sessionFactory;
+
+    static {
+        // 5.X版本，通过如下构建 SessionFactory
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+        sessionFactory = new MetadataSources(serviceRegistry).buildMetadata().buildSessionFactory();
+
+    }
+
+    /**
+     * 获取SessionFactory
+     */
+    private static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    /**
+     * 获取Session
+     */
+    public static Session getSession() {
+        return getSessionFactory().openSession();
+    }
+
+    /**
+     * 关闭Session
+     */
+    public static void closeSession(Session session) {
+        if (session != null) {
+            session.close();
+        }
+    }
 }
